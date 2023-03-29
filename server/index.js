a5cd7f215a7c91314332bdc1bf88570d643eaa8d
ikramkhan <ikramkhan951@gmail.com>
2023-03-30 01:23:30 +0500

    updated users.js in routes folder

--- server/index.js
+++ server/index.js
@@ -9,7 +9,9 @@ import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
 import authRoutes from "./routes/auth.js"
+import userRoutes from "./routes/users.js"
 import { register } from "./controllers/auth.js";
+import { verifyToken } from "./middleware/auth.js";
 
 
 /* CONFIGURATIONS */
@@ -42,6 +44,7 @@ app.post("/auth/register", upload.single("picture"), register);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
--- server/routes/users.js
+++ server/routes/users.js
@@ -6,3 +6,13 @@ import {
 } from "../controllers/users.js";
 import { verifyToken } from "../middleware/auth";
 
+const router = express.Router();
+
+/* READ */
+router.get("/:id", verifyToken, getUser);
+router.get("/:id/friends", verifyToken, getUserFriends);
+
+/* UPDATE */
+router.patch("/:id/:friendId", verifyToken, addRemoveFriend);
+
+export default router;
